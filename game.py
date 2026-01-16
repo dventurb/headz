7d6616f9190eea4c794c127000dadf12f5cb2573
dventurb <dventura@protonmail.com>
2026-01-16 23:46:34 +0000

    change the title

--- game.py
+++ game.py
@@ -3,6 +3,11 @@ import os
 
 
 def initialize_game():
-    sound = pg.mixer.Sound("assets/sounds/loop.mp3")
-    while(True):
-        sound.play()
+    pg.mixer.music.load("assets/sounds/loop.mp3")
+    pg.mixer.music.play(-1)
+
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                runnning = false
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from game import initialize_game
 WIDTH, HEIGHT = 900, 600
 
 def main():
-    pg.mixer.init()
+    pg.mixer.pre_init(44100, -16, 2, 2048)
     pg.init()
 
-# Set the display mode
+    # Set the display mode
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+    # Title 
+    pg.display.set_caption("Headz")
 
     initialize_game()
 
