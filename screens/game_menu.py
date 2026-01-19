5f66ceb46d8e61dfa58e6739a43eebf9b4dfba44
dventurb <dventura@protonmail.com>
2026-01-19 01:18:34 +0000

    basic movement player

--- screens/game_menu.py
+++ screens/game_menu.py
@@ -26,11 +26,18 @@ class GameMenu:
         if self.player is None or self.opponent is None:
             self.player = self.gameStateManager.selected_player
             self.opponent = self.gameStateManager.opponent
-            
+           
+           # TODO: Position y for the jump
             self.players = {
-                "player": Image(self.player.side_right, (384, 800)),
-                "opponent": Image(self.opponent.side_left, (1152, 800))
-                }
+                    "player": {
+                        "sprite": Image(self.player.side_right, (384, 800)),
+                        "position": 384   
+                        },
+                    "opponent": {
+                        "sprite": Image(self.opponent.side_left, (1152, 800)),
+                        "position": 1152
+                        }
+                    }
 
         self.draw()
 
@@ -38,13 +45,34 @@ class GameMenu:
             pg.mixer.music.load("assets/sounds/portugal.mp3")
             pg.mixer.music.play(-1)
             self.play_music = True
-        
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            position = self.players["player"]["position"] - (self.player.speed / 10)
+            if position <= 0:
+                self.players["player"]["position"] = 0
+            elif position > 0:
+                self.players["player"]["position"] = position
+            update_position(self)
+        if keys[pg.K_RIGHT]:
+            position = self.players["player"]["position"] + (self.player.speed / 10)
+            if position >= WIDTH:
+                self.players["player"]["position"] = WIDTH
+            elif position < WIDTH:
+                self.players["player"]["position"] = position
+            update_position(self)
 
     def draw(self):
         self.screen.blit(self.stadium, (0, 0))
+    
+        self.players["player"]["sprite"].draw(self.screen)
+        self.players["opponent"]["sprite"].draw(self.screen)
 
-        self.players["player"].draw(self.screen)
-        self.players["opponent"].draw(self.screen)
 
+def update_position(self):
+    position = self.players["player"]["position"]
+
+    # TODO: Still need to do position for the jump
+    self.players["player"]["sprite"] = Image(self.player.side_right, (position, 800))
 
         
